bc7f61cd0107e77fc4a8900d1d3f4850f18252d2
tvy86488 <normanwilliam.billanova@lisholding.it>
2023-03-22 09:32:33 +0100

    fixed PaymentService

--- SagaCoreographyPattern/PaymentService/src/main/java/com/example/paymentservice/Payment.java
+++ SagaCoreographyPattern/PaymentService/src/main/java/com/example/paymentservice/Payment.java
@@ -1,5 +1,7 @@
 package com.example.paymentservice;
 
+import jakarta.persistence.*;
+
 @Entity
 public class Payment {
     @Id
@@ -11,7 +13,31 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private PaymentStatus status;
 
-    // Altri campi come importo, informazioni sulla carta di credito, ecc.
+    // Altri campi come importo, dettagli del pagamento, ecc.
 
-    // Getter e setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    // Getter e setter per gli altri campi
 }
--- SagaCoreographyPattern/PaymentService/src/main/java/com/example/paymentservice/PaymentController.java
+++ SagaCoreographyPattern/PaymentService/src/main/java/com/example/paymentservice/PaymentController.java
@@ -1,5 +1,13 @@
 package com.example.paymentservice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
